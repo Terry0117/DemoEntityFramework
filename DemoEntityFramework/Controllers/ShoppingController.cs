6f2a36e10a5e103064814a2e253348af16f42e1f
Terry0117 <jsps000106@gmail.com>
2024-06-06 16:29:07 +0800

    加入專案檔案。

---
+++ DemoEntityFramework/Controllers/ShoppingController.cs
@@ -0,0 +1,20 @@
+﻿using DemoEntityFramework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoEntityFramework.Controllers
+{
+    public class ShoppingController : Controller
+    {
+        // GET: Shopping
+        public ActionResult Index()
+        {
+            var db = new dbShoppingEntities();
+            var model = db.OrderDetail.Where(m => m.UserId == "Bonny456" && m.Price >50).ToList();
+            return View(model);
+        }
+    }
+}
